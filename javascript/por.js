da216b8b36ce2e66257bd773e01042f8fd935c3f
MinChan Lee <rlcks01537@gmail.com>
2023-02-25 17:31:44 +0900

    2023-02-25 fampla-js-clone-coding commit

--- javascript/por.js
+++ javascript/por.js
@@ -3,14 +3,19 @@ const PORList = document.getElementById("POR-list");
 const PORInput = document.querySelector("#POR-form input");
 const PORUsername = document.querySelector("#role-box input");
 
-let planOrRequest = [];
+const planOrRequest = [];
+const POR_KEY = "planOrRequest";
 
-function deletePOS(event) {
+function savePOR() {
+  localStorage.setItem(POR_KEY, JSON.stringify(planOrRequest));
+}
+
+function deletePOR(event) {
   const li = event.target.parentElement;
   li.remove();
 }
 
-function paintPOS(newPOS) {
+function paintPOR(newPOR) {
   const li = document.createElement("li");
   const user = document.createElement("span");
   const span = document.createElement("span");
@@ -20,16 +25,24 @@ function paintPOS(newPOS) {
   li.appendChild(user);
   li.appendChild(span);
   li.appendChild(button);
-  button.addEventListener("click", deletePOS);
-  span.innerText = newPOS;
+  button.addEventListener("click", deletePOR);
+  span.innerText = newPOR;
   PORList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newPOS = PORInput.value;
+  const newPOR = PORInput.value;
   PORInput.value = "";
-  paintPOS(newPOS);
+  planOrRequest.push(newPOR);
+  paintPOR(newPOR);
+  savePOR();
 }
 
 PORForm.addEventListener("submit", handleToDoSubmit);
+const savedPOR = localStorage.getItem(POR_KEY);
+
+if (savedPOR !== null) {
+  const parsedPOR = JSON.parse(savedPOR);
+  parsedPOR.forEach();
+}
--- javascript/role.js
+++ javascript/role.js
@@ -1,6 +1,7 @@
 const roleBoxForm = document.querySelector("#role-box");
 const roleBoxInput = document.querySelector("#role-box input");
 const greeting = document.querySelector("#greeting");
+const reset = document.querySelector("#reset");
 
 const HIDDEN_KEY = "hidden";
 const USER_KEY = "username";
@@ -8,7 +9,7 @@ const USER_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   roleBoxForm.classList.add(HIDDEN_KEY);
-  retry.classList.remove(HIDDEN_KEY);
+  reset.className = "";
   const username = roleBoxInput.value;
   localStorage.setItem(USER_KEY, username);
   appearGreeting(username);
@@ -19,7 +20,13 @@ function appearGreeting(savedUsername) {
   greeting.classList.remove(HIDDEN_KEY);
 }
 
+function resetUser() {
+  localStorage.clear();
+  location.reload();
+}
+
 roleBoxForm.addEventListener("submit", onLoginSubmit);
+reset.addEventListener("click", resetUser);
 const savedUsername = localStorage.getItem(USER_KEY);
 
 if (savedUsername === null) {
